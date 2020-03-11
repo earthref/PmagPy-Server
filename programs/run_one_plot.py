c3f48423bc025f41f27fd5cff6bdf7adecce00e6
njarboe <njarboe@gmail.com>
2020-03-11 05:38:15 +0000

    changed back to make_magic_plots.py

--- programs/run_one_plot.py
+++ programs/run_one_plot.py
@@ -8,29 +8,27 @@ import datetime
 def main():
     """
     NAME
-        run_batch_plots.py
+        run_one_plot.py
 
     DESCRIPTION
-        Used only the MagIC pmagpy plot sever. It will create a tmp directory in the location where it is run. 
-        Downloads all one MagIC data files from S3 and then creates plots using new_make_magic_plots.
+        Used only on the MagIC pmagpy plot sever. It will create a tmp directory in the location where it is run. 
+        Downloads one MagIC data files from S3 and then creates plots using make_magic_plots.
         Plots are uploaded to the S3 MagIC server and copied the local webserver for easy web access for debugging.
-        Skips files in the bad_files list that have data formatting or missing data issues.
 
     SYNTAX
-        run_batch_plots.py [command line options]
+        run_one_plot.py [command line options]
 
     OPTIONS
         -mcn [MagIC contribution number] MagIC contribution number of the data file.  
         -out [FILENAME] redirect stdout to this file. File will be appended to.
-        -sb [BUCKETNAME] name of the AWS S3 bucket that has the magic datafiles in it to make the plots from (source bucket).
-                         Default value - magic-contributions 
-        -pb [BUCKETNAME] name of the AWS S3 bucket to place the plots into (plot bucket).
-                         Default value - magic-plots 
+        -sb [BUCKETNAME] name of the AWS S3 bucket that has the magic datafiles in it to make the plots from (source bucket).  Default value - magic-contributions 
+        -pb [BUCKETNAME] name of the AWS S3 bucket to place the plots into (plot bucket).  Default value - magic-plots 
 
     OUTPUT:
-        plots created to 
+        plots created put on S3 
+
     EXAMPLE:
-        run_batch_plots.py
+        run_batch_plots.py -mcn 16769
 
     """
 
@@ -73,8 +71,14 @@ def main():
     printout= command + "\n"
     f.write(printout)
     os.system(command)
-    os.system("new_make_magic_plots.py")
-    os.system("rm locations.txt sites.txt samples.txt speciments.txt measurements.txt ages.txt criteria.txt")
+    command="make_magic_plots.py"
+    printout= command + "\n"
+    f.write(printout)
+    os.system(command)
+    command="rm locations.txt sites.txt samples.txt speciments.txt measurements.txt ages.txt criteria.txt"
+    printout= command + "\n"
+    f.write(printout)
+    os.system(command)
     command="aws s3 rm s3://" + plotBucket + "/" + mcn + " --recursive"
     printout= command + "\n"
     f.write(printout)
