5d77c5e660bd57316153d9f2d91ff4548fdf7f6b
njarboe <njarboe@gmail.com>
2020-03-11 14:48:07 +0000

    change back to make_magic_plots.py

--- programs/create_new_plots.py
+++ programs/create_new_plots.py
@@ -28,6 +28,8 @@ def main():
        -p the time in seconds to check in the past for a new file to process. Default 35.
        -w the time in seconds to wait after processing before checking again. Default 30.
        -a add the time the last cycle took to run to the -p value so it is less likely files are missed.
+       -pt [TYPE] set to either 'public' or 'private'. This determines if the program looks for and
+           processes private contributions or public ones. Default 'public'.
        -out [FILENAME] redirect stdout to this file. File will be appended to.
 
     OUTPUT:
@@ -45,6 +47,7 @@ def main():
     addTime=False
     commandLength = timedelta(seconds=1)
     startTime=datetime.datetime.now() 
+    bucketName=''
     if '-h' in sys.argv: # check if help is needed
         print(main.__doc__)
         sys.exit() # graceful quit
@@ -62,6 +65,13 @@ def main():
         f = open(logFile, "a")
     else:
         f = sys.stdout
+    if '-pt' in sys.argv:
+        ind=sys.argv.index('-pt')
+        bucketName=''
+        if sys.argv[ind+1] == 'private':
+            bucketName='private-'
+        elif sys.argv[ind+1] != 'public':
+            print ('Unknown contribution type ',sys.argv[ind+1], '. "public" or "private" are acceptable types.')
     while(True):
         startTime=datetime.datetime.now() 
         d = timedelta(seconds=past)
@@ -77,7 +87,7 @@ def main():
         else:
             pastTime=startTime-d
 
-        command='aws s3api list-objects --bucket "magic-contributions" --query' +" 'Contents[?LastModified>=`" + pastTime.isoformat() + "`][].{Key: Key, LastModified: LastModified}' > fileList" 
+        command='aws s3api list-objects --bucket "magic-' + bucketName +'contributions" --query' +" 'Contents[?LastModified>=`" + pastTime.isoformat() + "`][].{Key: Key, LastModified: LastModified}' > fileList" 
         printout="command=" + command + "\n"
         f.write(printout)
         os.system(command)
@@ -92,14 +102,14 @@ def main():
                 printout="fileName=" + fileName + "\n"
                 f.write(printout)
                 if ".txt" in fileName:
-                    command='aws s3 cp s3://magic-contributions/' + fileName +' ' + fileName
+                    command='aws s3 cp s3://magic-' + bucketName + 'contributions/' + fileName +' ' + fileName
                     printout="command=" + command + "\n"
                     f.write(printout)
                     os.system(command)
                     splitline=fileName.split('/')
                     magicId=splitline[0]
                     # clear the bucket for Website "Making Plots" display purposes
-                    command='aws s3 mb s3://magic-plots/' +magicId  
+                    command='aws s3 mb s3://magic-' + bucketName + 'plots/' +magicId  
                     f.write(command+'\n')
                     os.system(command) 
                     contribId=splitline[1]
@@ -114,13 +124,13 @@ def main():
                     command='cp -rf ' + magicId + ' /var/www/html/plots' 
                     f.write(command+'\n')
                     os.system(command) 
-                    command='aws s3 rm s3://magic-plots/' +magicId + ' --recursive' 
+                    command='aws s3 rm s3://magic-' + bucketName + 'plots/' +magicId + ' --recursive' 
                     f.write(command+'\n')
                     os.system(command) 
-                    command='aws s3 cp ' + magicId + ' s3://magic-plots/' +magicId + ' --recursive --include "*.png"'
+                    command='aws s3 cp ' + magicId + ' s3://magic' + bucketName + '-plots/' +magicId + ' --recursive --include "*.png"'
                     f.write(command+'\n')
                     os.system(command) 
-                    command='aws s3 cp ' + magicId + ' s3://magic-plots/' +magicId + ' --recursive --include "contributions.txt" --include "errors.txt"' 
+                    command='aws s3 cp ' + magicId + ' s3://magic' + bucketName + '-plots/' +magicId + ' --recursive --include "contributions.txt" --include "errors.txt"' 
                     f.write(command+'\n')
                     os.system(command) 
                     command='rm -r processed/' + magicId
@@ -138,7 +148,7 @@ def main():
             w=wait-commandLength.total_seconds()
             if w<0:
                     w=0
-                    printout="Warning: make_magic_plots took longer to run than the wait time.\n"
+                    printout="Warning: new_make_magic_plots took longer to run than the wait time.\n"
                     printout=printout +"Checking S3 for new MagIC data files immediately.\n"
                     f.write(printout)
             printout = "\nsleep will be " +str(w)+ " seconds\n"
